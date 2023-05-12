c8df5e6c5cfebd7af4339cf40d882b600327339b
karthiksatish <karthiksatish159@gmail.com>
2023-05-12 14:52:12 +0530

    Running analyisis on data and generating a report is done

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+}).split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+let manUnitedWinsCount = 0;
+for (let value of matches) {
+    if (value[1] === 'Man United' && value[5] === 'H') {
+        manUnitedWinsCount++;
+    }
+    else if (value[2] === 'Man United' && value[5] === 'A') {
+        manUnitedWinsCount++;
+    }
+}
+console.log(manUnitedWinsCount);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
 import fs from 'fs';
+const matches=fs.readFileSync('football.csv',
+{
+    encoding:'utf-8'
+}).split('\n')
+  .map((row:string):string[]=>
+  {
+    return row.split(',');
+  });
+let manUnitedWinsCount:number=0;
+for(let value of matches)
+{
+    if(value[1]==='Man United'&&value[5]==='H')
+    {
+        manUnitedWinsCount++;
+    }
+    else if(value[2]==='Man United'&&value[5]==='A'){manUnitedWinsCount++}
+}
+console.log(manUnitedWinsCount);
