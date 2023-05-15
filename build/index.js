e882102c7402e3556d1e869253bd8ddd3b140843
karthiksatish <karthiksatish159@gmail.com>
2023-05-15 11:28:10 +0530

    enums are added to enumarete the set of constants

--- build/index.js
+++ build/index.js
@@ -10,12 +10,18 @@ const matches = fs_1.default.readFileSync('football.csv', {
     .map((row) => {
     return row.split(',');
 });
+var MatchResults;
+(function (MatchResults) {
+    MatchResults["HomeWin"] = "H";
+    MatchResults["AwayWin"] = "A";
+    MatchResults["Draw"] = "D";
+})(MatchResults || (MatchResults = {}));
 let manUnitedWinsCount = 0;
 for (let value of matches) {
-    if (value[1] === 'Man United' && value[5] === 'H') {
+    if (value[1] === 'Man United' && value[5] === MatchResults.HomeWin) {
         manUnitedWinsCount++;
     }
-    else if (value[2] === 'Man United' && value[5] === 'A') {
+    else if (value[2] === 'Man United' && value[5] === MatchResults.AwayWin) {
         manUnitedWinsCount++;
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,19 @@ const matches=fs.readFileSync('football.csv',
   {
     return row.split(',');
   });
+enum MatchResults
+{
+  HomeWin='H',
+  AwayWin='A',
+  Draw='D'
+}
 let manUnitedWinsCount:number=0;
 for(let value of matches)
 {
-    if(value[1]==='Man United'&&value[5]==='H')
+    if(value[1]==='Man United'&&value[5]===MatchResults.HomeWin)
     {
         manUnitedWinsCount++;
     }
-    else if(value[2]==='Man United'&&value[5]==='A'){manUnitedWinsCount++}
+    else if(value[2]==='Man United'&&value[5]===MatchResults.AwayWin){manUnitedWinsCount++}
 }
 console.log(manUnitedWinsCount);
